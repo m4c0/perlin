8068af37cea81919ecc4ee107ef00c26d8d30cb6
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-29 21:19:13 +0100

    trying to implement the old algo

--- poc.cpp
+++ poc.cpp
@@ -26,8 +26,36 @@ static inline void init() {
   }
 }
 
-static inline float noise(dotz::vec2 p) {
-  return 1.0;
+static inline constexpr auto s_curve(float t) {
+  return t * t * (3.0f - 2.0f * t);
+}
+
+static inline float noise(dotz::vec2 pos) {
+  dotz::vec2 t = pos + 0x1000;
+  dotz::ivec2 ti { static_cast<int>(t.x), static_cast<int>(t.y) };
+  dotz::ivec2 b0 { ti.x & 0xFF, ti.y & 0xFF };
+  dotz::ivec2 b1 { (ti.x + 1) & 0xFF, (ti.y + 1) & 0xFF };
+  auto r0 = t - ti;
+  auto r1 = r0 - 1;
+  
+  auto i = p[b0.x];
+  auto j = p[b1.x];
+
+  auto g00 = g[p[i + b0.y]];
+  auto g10 = g[p[j + b0.y]];
+  auto g01 = g[p[i + b1.y]];
+  auto g11 = g[p[j + b1.y]];
+
+  dotz::vec2 s { s_curve(r0.x), s_curve(r0.y) };
+
+  auto u = dotz::mix(s.x,
+      r0.x * g00.x + r0.y * g00.y,
+      r1.x * g10.x + r0.y * g10.y);
+  auto v = dotz::mix(s.x,
+      r0.x * g01.x + r1.y * g01.y,
+      r1.x * g11.x + r1.y * g11.y);
+
+  return dotz::mix(s.y, u, v);
 }
 
 int main() {
