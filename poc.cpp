51525ca1fa51f4074264b1be2ca7563314aa7e23
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-29 23:18:11 +0100

    scaling the output

--- poc.cpp
+++ poc.cpp
@@ -68,7 +68,7 @@ int main() {
 
   for (auto y = 0; y < 1024; y++) {
     for (auto x = 0; x < 1024; x++) {
-      int c = noise({ x, y }) * 128 + 127;
+      int c = noise(dotz::vec2 { x, y } / 32.0) * 128 + 127;
       auto cc = static_cast<unsigned char>(c & 0xFF);
       img[y * 1024 + x] = { cc, cc, 255, 255 };
     }
